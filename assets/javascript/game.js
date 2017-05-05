08470a394c1b143e7b36964ddfa21b792c830774
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-05-05 15:26:31 -0500

    message

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+
+var game = {
+
+	crystals: {
+
+		green: {
+			value:
+			image: "assets/images/green.png"
+		}
+
+		purple: {
+			value:
+			image: "assets/images/purple.png"
+		}
+
+		blue: {
+			value: 
+			image: "assets/images/blue.png"
+		}
+
+		yellow: {
+			value: 
+			image: "assets/images/yellow.png"
+		}
+
+
+	}
+
+
+
+
+
+
+}
